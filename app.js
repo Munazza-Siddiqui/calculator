0d80f513ac8e74919a0f9e86e5e1ae7f212b3c2f
Munazza-Siddiqui <munazzasiddiqui22@gmail.com>
2020-06-28 22:57:08 +0500

    ready

---
+++ app.js
@@ -0,0 +1,9 @@
+function getNumber(num){
+    var calculation= document.getElementById("calculation");
+    calculation.value +=num;
+
+}
+function del(){
+    var calculation= document.getElementById("calculation");
+    calculation.value +="";
+}
