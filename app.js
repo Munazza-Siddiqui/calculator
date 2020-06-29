4cd6c6445b22bf45d8915d4a9c68b6828e92c20d
Munazza-Siddiqui <64565979+Munazza-Siddiqui@users.noreply.github.com>
2020-06-29 06:28:06 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-function getNumber(num){
-    var calculation= document.getElementById("calculation");
-    calculation.value +=num;
+function getNumber(a){
+    var result=document.getElementById("cal");
+    result.value += a;
+}
+
+function clearResult(){
+    var result= document.getElementById("cal");
+    result.value="";
+}
+function getResult(){
+    var result = document.getElementById("cal");
+    result.value = eval(result.value);
 
 }
-function del(){
-    var calculation= document.getElementById("calculation");
-    calculation.value +="";
+function back(){
+    var result = document.getElementById("cal");
+    result.value=result.value.slice(0,-1);
 }
+function sqaure(){
+    var result = document.getElementById("cal");
+    result.value =result.value*result.value;
+}
+function sqrrt(){
+    var result = document.getElementById("cal");
+    result.value =Math.sqrt(result.value); 
+}
+
+
+
+
